13e2bf9d1494ed6854128707e1a62ed9de2dc0ff
AkashMundari <153832855+AkashMundari@users.noreply.github.com>
2024-08-06 23:07:02 +0530

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -4,11 +4,11 @@ import Triple from "../assets/triple.png";
 const Pricing = () => {
   return (
     <>
-      <div className="bg-white w-full p-20">
-        <div className="max-w-[1200px] grid md:grid-cols-3 gap-8 mx-auto">
+      <div className="bg-white w-full p-8">
+        <div className="max-w-full grid md:grid-cols-3 gap-8 mx-auto">
           <div className="flex flex-col justify-center items-center border-2 p-5 rounded-sm shadow-md  bg-white hover:scale-105 duration-150">
             <img className="w-20 h-15" src={Single} alt="Single" />
-            <h1 className="text-3xl font-bold mt-4">Single User</h1>
+            <h1 className="text-3xl font-bold mt-4 text-center">Single User</h1>
             <p className="text-4xl mt-3 mb-3 font-extrabold">$149</p>
             <p className="font-bold mt-2 border-b-2">500 GB Storage</p>
             <p className="font-bold mt-2 border-b-2">1 Granted User</p>
@@ -19,7 +19,7 @@ const Pricing = () => {
           </div>
           <div className="flex flex-col justify-center items-center border-2 p-5 rounded-sm shadow-md  bg-white hover:scale-105 duration-150">
             <img className="w-20 h-15" src={Double} alt="Single" />
-            <h1 className="text-3xl font-bold mt-4">Double User</h1>
+            <h1 className="text-3xl font-bold mt-4 text-center">Double User</h1>
             <p className="text-4xl mt-3 mb-3 font-extrabold">$249</p>
             <p className="font-bold mt-2 border-b-2">500 GB Storage</p>
             <p className="font-bold mt-2 border-b-2">1 Granted User</p>
@@ -30,7 +30,7 @@ const Pricing = () => {
           </div>
           <div className="flex flex-col justify-center items-center border-2 p-5 rounded-sm shadow-md  bg-white hover:scale-105 duration-150">
             <img className="w-20 h-15" src={Triple} alt="Single" />
-            <h1 className="text-3xl font-bold mt-4">Triple User</h1>
+            <h1 className="text-3xl font-bold mt-4 text-center">Triple User</h1>
             <p className="text-4xl mt-3 mb-3 font-extrabold">$349</p>
             <p className="font-bold mt-2 border-b-2">500 GB Storage</p>
             <p className="font-bold mt-2 border-b-2">1 Granted User</p>
